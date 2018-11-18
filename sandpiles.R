22f9d66e6cd7ae27f5c108ccdc272fe200cf8314
BastiHz <41594266+BastiHz@users.noreply.github.com>
2018-11-18 13:33:35 +0100

    Make sandpile toppling much faster and increase size of pile automatically

--- sandpiles.R
+++ sandpiles.R
@@ -6,41 +6,52 @@
 # Lots of sand is put in the center and then the rules are applied until there
 # are no more stacks to topple.
 
-# TODO:
-# - Increase the size of the matrix each time the sand reaches the edge.
-# - Make it faster so it can handle more sand.
-
 
 library(grid)
 
 
-sandpile <- matrix(0, nrow = 101, ncol = 101)
-sandpile[51, 51] <- 15000
+sidelength <- 31  # must be an odd number
+height <- 30000
 
+n <- sidelength ^ 2
+sandpile <- matrix(0, nrow = sidelength, ncol = sidelength)
+center <- ceiling(sidelength / 2)
+sandpile[center, center] <- height
+left_border <- seq(1, sidelength)
+increase_by <- 10  # must be an even number
 
 repeat {
-    to_topple <- which(sandpile > 3, arr.ind = TRUE)
-    if (nrow(to_topple) == 0) break
+    to_topple <- which(sandpile > 3)
+    if (length(to_topple) == 0) break
+
+    if (any(to_topple %in% left_border)) {
+        # Increase the size of the pile
+        new_sidelength <- sidelength + 2 * increase_by
+        new_pile <- matrix(0, nrow = new_sidelength, ncol = new_sidelength)
+        middle <- seq(increase_by + 1, length.out = sidelength)
+        coords <- cbind(
+            row = rep(middle, sidelength),
+            col = rep(middle, each = sidelength)
+        )
+        new_pile[coords] <- sandpile
+        sandpile <- new_pile
+        sidelength <- new_sidelength
+        n <- sidelength ^ 2
+        left_border <- seq(1, sidelength)
+        to_topple <- which(sandpile > 3)
+    }
 
     sandpile[to_topple] <- sandpile[to_topple] - 4
-    rows <- to_topple[, "row"]
-    cols <- to_topple[, "col"]
-    neighbors <- cbind(
-        row = c(rows - 1, rows, rows, rows + 1),
-        col = c(cols, cols - 1, cols + 1, cols)
-    )
-    on_the_grid <- (
-        neighbors[, "row"] > 0 & neighbors[, "row"] <= nrow(sandpile) &
-        neighbors[, "col"] > 0 & neighbors[, "col"] <= ncol(sandpile)
-    )
-    neighbors <- neighbors[on_the_grid, ]
 
-    # The following step consumes all the computation time. I tried to improve
-    # it but this seems to be the fastest I can make it.
-    for (i in 1:nrow(neighbors)) {
-        position <- neighbors[i, , drop = FALSE]
-        sandpile[position] <- sandpile[position] + 1
-    }
+    top_neighbors <- to_topple - 1
+    bottom_neighbors <- to_topple + 1
+    left_neighbors <- to_topple - sidelength
+    right_neighbors <- to_topple + sidelength
+
+    sandpile[top_neighbors] <- sandpile[top_neighbors] + 1
+    sandpile[bottom_neighbors] <- sandpile[bottom_neighbors] + 1
+    sandpile[left_neighbors] <- sandpile[left_neighbors] + 1
+    sandpile[right_neighbors] <- sandpile[right_neighbors] + 1
 }
 
 sand_colors <- data.frame(
@@ -50,4 +61,7 @@ sand_colors <- data.frame(
 )
 color_matrix <- matrix(sand_colors$color[match(sandpile, sand_colors$num)],
                        nrow = nrow(sandpile), ncol = ncol(sandpile))
+png(paste0("sandpile_", height, ".png"),
+    width = ncol(sandpile), height = nrow(sandpile))
 grid.raster(color_matrix, interpolate = FALSE)
+dev.off()
