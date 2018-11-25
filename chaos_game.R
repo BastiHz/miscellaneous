20e6dddb5bdc334db115e38d9920eb1f62ad755f
BastiHz <41594266+BastiHz@users.noreply.github.com>
2018-11-25 19:16:06 +0100

    Rename num_iterations to num_points

--- chaos_game.R
+++ chaos_game.R
@@ -8,7 +8,7 @@
 
 
 chaos_polygon <- function(n = 3, move_distance = 0.5,
-                          num_iterations = 1000, plot = TRUE, ...) {
+                          num_points = 1000, plot = TRUE, ...) {
     # Start with the corners of a regular polygon. Place a tracepoint somewhere
     # between those. Each step of the iteration choose a random corner and move
     # the tracepoint some distance towards that. Vary the number of corners and
@@ -27,12 +27,12 @@ chaos_polygon <- function(n = 3, move_distance = 0.5,
     }
 
     result <- data.frame(
-        x = numeric(num_iterations),
-        y = numeric(num_iterations)
+        x = numeric(num_points),
+        y = numeric(num_points)
     )
-    targets <- sample(1:n, num_iterations, replace = TRUE)
+    targets <- sample(1:n, num_points, replace = TRUE)
     tracepoint <- corners[1, ]
-    for (i in seq_len(num_iterations)) {
+    for (i in seq_len(num_points)) {
         tracepoint <-
             tracepoint -
             (tracepoint - corners[targets[i], ]) *
@@ -48,8 +48,8 @@ chaos_polygon <- function(n = 3, move_distance = 0.5,
 
 # chaos_polygon()
 # chaos_polygon(n = 4, move_distance = 0.6)
-# chaos_polygon(n = 5, move_distance = 0.62, num_iterations = 3000)
+# chaos_polygon(n = 5, move_distance = 0.62, num_points = 3000)
 # chaos_polygon(n = 6, move_distance = 2/3)
 # chaos_polygon(n = 4, type = "l")
-# chaos_polygon(30, move_distance = 0.91, num_iterations = 2000)
+# chaos_polygon(30, move_distance = 0.91, num_points = 2000)
 # chaos_polygon(2, move_distance = 0.7)
