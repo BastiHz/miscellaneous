05263fc22a66a50f1facc6de4b483a681bda58b3
BastiHz <41594266+BastiHz@users.noreply.github.com>
2018-11-25 20:40:22 +0100

    Incredible performance improvements

--- chaos_game.R
+++ chaos_game.R
@@ -4,11 +4,9 @@
 # Inspired by this Numberphile video:
 # https://www.youtube.com/watch?v=kbKtFN71Lfs
 
-# TODO: Implement Barnsley's Fern
-
 
 chaos_polygon <- function(n = 3, move_distance = 0.5,
-                          num_points = 1000, plot = TRUE, ...) {
+                          num_points = 10000, plot = TRUE, ...) {
     # Start with the corners of a regular polygon. Place a tracepoint somewhere
     # between those. Each step of the iteration choose a random corner and move
     # the tracepoint some distance towards that. Vary the number of corners and
@@ -26,19 +24,26 @@ chaos_polygon <- function(n = 3, move_distance = 0.5,
         corners$y[i] <- cos(radians[i])
     }
 
-    result <- data.frame(
-        x = numeric(num_points),
-        y = numeric(num_points)
-    )
+    # This version of the code below came to be after many iterations of
+    # preformance improvements. At first I did everything using data frames
+    # which took around 1 s for 1000 points. But using vectors like shown here
+    # and creating the result data frame afterwards makes everything extremely
+    # fast. Now it runs 1 million points in less than half a second. The new
+    # bottleneck with so many points is the plot() function.
+    trace_x <- corners$x[1]
+    trace_y <- corners$y[1]
+    result_x <- result_y <- numeric(num_points)
     targets <- sample(1:n, num_points, replace = TRUE)
-    tracepoint <- corners[1, ]
+    ct_x <- corners$x[targets]
+    ct_y <- corners$y[targets]
     for (i in seq_len(num_points)) {
-        tracepoint <-
-            tracepoint -
-            (tracepoint - corners[targets[i], ]) *
-            move_distance
-        result[i, ] <- tracepoint
+        trace_x <- trace_x - (trace_x - ct_x[i]) * move_distance
+        trace_y <- trace_y - (trace_y - ct_y[i]) * move_distance
+        result_x[i] <- trace_x
+        result_y[i] <- trace_y
     }
+    result <- data.frame(x = result_x, y = result_y)
+
     if (!plot) return(result)
     opar <- par(no.readonly = TRUE)
     on.exit(par(opar))
@@ -47,9 +52,10 @@ chaos_polygon <- function(n = 3, move_distance = 0.5,
 }
 
 # chaos_polygon()
-# chaos_polygon(n = 4, move_distance = 0.6)
-# chaos_polygon(n = 5, move_distance = 0.62, num_points = 3000)
-# chaos_polygon(n = 6, move_distance = 2/3)
-# chaos_polygon(n = 4, type = "l")
+# chaos_polygon(num_points = 50000, cex = 0.2)
+# chaos_polygon(n = 4, move_distance = 0.55, num_points = 20000, cex = 0.25)
+# chaos_polygon(n = 5, move_distance = 0.62, num_points = 30000, cex = 0.2)
+# chaos_polygon(n = 6, move_distance = 2/3, cex = 0.2, num_points = 50000)
+# chaos_polygon(n = 4, type = "l", num_points = 1000)
 # chaos_polygon(30, move_distance = 0.91, num_points = 2000)
 # chaos_polygon(2, move_distance = 0.7)
