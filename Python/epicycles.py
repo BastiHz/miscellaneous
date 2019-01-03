70a85407129e11b770daa0a7e6e2d506bb2ab678
BastiHz <41594266+BastiHz@users.noreply.github.com>
2019-01-03 14:57:17 +0100

    Add R code comment as inspiration for later

--- Python/epicycles.py
+++ Python/epicycles.py
@@ -3,7 +3,35 @@ License: MIT (see file LICENSE for details)
 
 """
 
-# TODO: Make radius a float to allow for very small circles.
+# TODO:
+# - Make radius a float to allow for very small circles.
+# - Use complex numbers.
+# - When drawing very small circles either draw them with radius=1 or don't draw them.
+# - Calculate the parameters of the circles for a path using fourier analysis.
+# - Use command line arguments.
+
+# Some R code as inspiration for later:
+# https://www.youtube.com/watch?v=qS4H6PEcCCA
+#
+# t <- seq(0, 2 * pi, length.out = 100)
+# 
+# p1 <- (10.72 + 16.52i) * exp(0i * t)
+# p2 <- (-12.64 + 20.90i) * exp(1i * t)
+# p3 <- (-135.66 - 45.57i) * exp(-1i * t)
+# p4 <- (-44.85 - 23.71i) * exp(2i * t)
+# p5 <- (66.75 - 53.07i) * exp(-2i * t)
+# p <- p1 + p2 + p3 + p4 + p5
+# par(pty = "s")
+# plot(p, pch = 20, asp = 1)
+# 
+# points(p1, pch = 4)
+# 
+# plot(p4)
+# points(0, abs(-44.85 - 23.71i), pch = 4)
+#
+# Die erste komplexe Zahl ist der Radius. Das t ist die Phase von 0 bis 2pi.
+# Die Zahl vor dem i im exp() ist die Richtung und Anzahl der Rotationen.
+# Bei p1 rotiert nichts. Das ist glaube ich die Mitte des ersten Kreises.
 
 
 import os
