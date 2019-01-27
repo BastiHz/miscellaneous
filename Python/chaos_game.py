0ebb8baa2bc572bba03d99dfa07a2dcf7ccfe33c
BastiHz <41594266+BastiHz@users.noreply.github.com>
2019-01-27 15:58:51 +0100

    Change license from MIT to GNU GPL v3

--- Python/chaos_game.py
+++ Python/chaos_game.py
@@ -1,12 +1,25 @@
-"""Sebastian Henz (2018)
-License: MIT (see file LICENSE for details)
-
+"""This is the animated companion script to chaos_game.R. It only does
+the polygons and not the fern.
 Inspired by this Numberphile video:
 https://www.youtube.com/watch?v=kbKtFN71Lfs
 
-This is the animated companion script to chaos_game.R. It only does
-the polygons and not the fern.
+
+Copyright (C) 2019 Sebastian Henz
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
+
 
 import math
 import os
--- R/chaos_game.R
+++ R/chaos_game.R
@@ -1,12 +1,25 @@
-# Sebastian Henz (2018)
-# License: MIT (see file LICENSE for details)
-#
 # Inspired by this Numberphile video:
 # https://www.youtube.com/watch?v=kbKtFN71Lfs
 #
 # This script includes the polygons made from random points and the fern shown
 # in the video. For an animated version of the polygons see chaos_game.py. For
 # more info on the fern see https://en.wikipedia.org/wiki/Barnsley_fern
+#
+#
+# Copyright (C) 2019 Sebastian Henz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 
 chaos_polygon <- function(n = 3, move_distance = 0.5,
