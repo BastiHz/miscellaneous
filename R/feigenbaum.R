e48558d49f7825a223a212e2fc3ec8860eb231bf
BastiHz <41594266+BastiHz@users.noreply.github.com>
2019-02-14 19:04:21 +0100

    Improve feigenbaum.R

--- R/feigenbaum.R
+++ R/feigenbaum.R
@@ -18,8 +18,7 @@
 # https://www.youtube.com/watch?v=ETrYE4MdoLQ
 
 
-
-f <- function(lambda, precision = 7, max_iterations = 10000, start = 0.5,
+f <- function(lambda, precision = 5, max_iterations = 10000, start = 0.5,
               max_return = 500) {
     # lambda: A number in the interval [0, 4].
     # precision: The number of decimal places the numbers are rounded to.
@@ -28,12 +27,10 @@ f <- function(lambda, precision = 7, max_iterations = 10000, start = 0.5,
     # start: The starting value.
     # max_return: The maximum length of the vector returned.
 
-    # Note: The values are rounded at each step. I could improve the
-    # algorithm by comparing rounded numbers but keeping x and n precise.
-
     n <- numeric(max_iterations)
     x <- round(start, precision)
-    n[1] <- x
+    x_rounded <- round(x, precision)
+    n[1] <- x_rounded
 
     if (max_iterations <= 2) {
         stop("max_iterations must be > 2.")
@@ -43,42 +40,37 @@ f <- function(lambda, precision = 7, max_iterations = 10000, start = 0.5,
     }
 
     for (i in 2:max_iterations) {
-        x <- round(lambda * x * (1 - x), precision)
-        if (x == 0) return(0)
-        if (x %in% n) {
+        x <- lambda * x * (1 - x)
+        x_rounded <- round(x, precision)
+        if (x_rounded == 0) return(0)
+        if (x_rounded %in% n) {
             n <- n[n > 0]
-            n <- n[which(n == x):length(n)]
+            n <- n[which(n == x_rounded):length(n)]
             if (length(n) <= max_return) return(n)
             break
         }
-        n[i] <- x
+        n[i] <- x_rounded
     }
-    from <- max_iterations - max_return + 1
-    return(n[from:max_iterations])
+    from <- length(n) - max_return + 1
+    return(n[from:length(n)])
 }
 
-# Initialise x and y to make it faster to add the results to them.
-# TODO: Trim the tails containing zeros afterwards.
 
+# I initialize x and y empty and cut off the rest later to increase the speed.
 len <- 1000
 max_return <- 500
 x <- y <- numeric(len * max_return)
 i <- 1
-for (lambda in seq(3, 4, length.out = len)) {
+for (lambda in seq(3.4, 4, length.out = len)) {
     cycle <- f(lambda, max_return = max_return)
     len_cycle <- length(cycle)
-    j <- i:(i + len_cycle - 1)
+    j <- seq(i, i + len_cycle - 1)
     x[j] <- rep(lambda, len_cycle)
     y[j] <- cycle
+    i <- tail(j, 1) + 1
 }
-# f(3) springt zwischen 0.668 und 0.664 hin und her. Ich sollte doch lieber
-# gerundete Zahlen vergleichen und die precision auf sowas wie 3 festlegen. Bei
-# der Darstellung macht mehr Genauigkeit keinen Sinn, wenn man die Punkte
-# sowieso nicht voneinander unterscheiden kann. Aber das mit der max_return
-# werde ich behalten. Und die eigentliche Berechnung wird noch mit einem
-# präzisen x durchgeführt. Aber es wird gerundet, bevor es zu n hinzugefügt
-# wird.
-# Überlänge von x und y abschneiden.
-
+keep <- seq(1, max(which(x > 0)))
+x <- x[keep]
+y <- y[keep]
 length(x)
 plot(x, y, pch = ".")
