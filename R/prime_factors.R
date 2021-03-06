3af155fa95f4de1c1ae2da0f28e2d72c726581e5
bastihz <bastihz.dev@posteo.de>
2021-03-06 13:25:21 +0100

    update prime_factors.R

--- R/prime_factors.R
+++ R/prime_factors.R
@@ -2,17 +2,31 @@
 
 get_prime_factors <- function(n) {
     stopifnot("n must be integer" = n %% 1 == 0)
-    prime_factors <- integer()
-    i <- 2L
+    prime_factors <- numeric()
+    i <- 2
     while (n > 1) {
         while (n %% i == 0) {
             prime_factors <- c(prime_factors, i)
-            n <- n %/% i
+            n <- n / i
         }
-        i <- i + 1L
+        i <- i + 1
+    }
+    if (length(prime_factors) == 0) {
+        return(n)
     }
     prime_factors
 }
 
-p <- get_prime_factors(123456789L)
-typeof(p)
+p <- get_prime_factors(123456789)
+p
+prod(p)
+
+p <- get_prime_factors(1000001)
+p
+prod(p)
+
+p <- get_prime_factors(1000003)  # prime number
+p
+prod(p)
+
+get_prime_factors(2**30)
